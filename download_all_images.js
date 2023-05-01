fb185fd1428c227ffd927b3d06144097c5029c6f
matthew rendely <matthew.rendely@gmail.com>
2023-04-30 22:14:34 -0700

    fix indents and add javascript: to download bookmarklet

--- download_all_images.js
+++ download_all_images.js
@@ -1,35 +1,34 @@
-(function() {
+javascript: (function () {
   var images = [].slice.call(document.querySelectorAll('img'));
   try {
-    images.forEach(function(img, i) {
-	  if (img.width >= 200) setTimeout(() => downloadResource(img.src),i*500)
+    images.forEach(function (img, i) {
+      if (img.width >= 200) setTimeout(() => downloadResource(img.src), i * 500)
     })
   } catch (e) {
-      alert("Download failed.");
-      console.log('Download failed.', e);
+    alert("Download failed.");
+    console.log('Download failed.', e);
   }
-  
-  function forceDownload(blob, filename) {
-  var a = document.createElement('a');
-  a.download = filename;
-  a.href = blob;
-  a.click();
-}
 
-function downloadResource(	url, filename) {
-  if (!filename) filename = url.split('\\').pop().split('/').pop();
-  fetch(url, {
+  function forceDownload(blob, filename) {
+    var a = document.createElement('a');
+    a.download = filename;
+    a.href = blob;
+    a.click();
+  }
+
+  function downloadResource(url, filename) {
+    if (!filename) filename = url.split('\\').pop().split('/').pop();
+    fetch(url, {
       headers: new Headers({
         'Origin': location.origin
       }),
       mode: 'cors'
     })
-    .then(response => response.blob())
-    .then(blob => {
-      let blobUrl = window.URL.createObjectURL(blob);
-      forceDownload(blobUrl, filename);
-    })
-    .catch(e => console.error(e));
-}
-
+      .then(response => response.blob())
+      .then(blob => {
+        let blobUrl = window.URL.createObjectURL(blob);
+        forceDownload(blobUrl, filename);
+      })
+      .catch(e => console.error(e));
+  }
 }).call(window);
